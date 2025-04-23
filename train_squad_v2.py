de3ffcfd5101d74ac72ec784b4cbcd8195ee3dd2
Phuong <phuong9agt@gmail.com>
2025-04-23 20:40:00 +0700

    Update train_squad_v2.py

--- train_squad_v2.py
+++ train_squad_v2.py
@@ -2,11 +2,9 @@ import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
 
 from huggingface_hub import login
-# login(token="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", add_to_git_credential=True)
+# login(token="hf_xxx", add_to_git_credential=True)
 
 from typing import Union, Any, Dict
-# from datasets.arrow_dataset import Batch
-
 import argparse
 import datasets
 from transformers.utils import logging, check_min_version
@@ -16,13 +14,10 @@ from retro_reader import RetroReader
 from retro_reader.constants import EXAMPLE_FEATURES
 import torch
 
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.13.0.dev0")
-
 require_version("datasets>=1.8.0")
 
 logger = logging.get_logger(__name__)
-
 
 def schema_integrate(example) -> Union[Dict, Any]:
     title = example["title"]
@@ -39,7 +34,6 @@ def schema_integrate(example) -> Union[Dict, Any]:
         else:
             answers.append({"text": [""], "answer_start": [-1]})
             is_impossible.append(True)
-    # The feature names must be sorted.
     return {
         "guid": guid,
         "question": question,
@@ -52,26 +46,19 @@ def schema_integrate(example) -> Union[Dict, Any]:
         "dataset": dataset_name,
     }
 
-
-# data augmentation for multiple answers
 def data_aug_for_multiple_answers(examples) -> Union[Dict, Any]:
     result = {key: [] for key in examples.keys()}
-    
+
     def update(i, answers=None):
         for key in result.keys():
             if key == "answers" and answers is not None:
                 result[key].append(answers)
             else:
                 result[key].append(examples[key][i])
-                
-    for i, (answers, unanswerable) in enumerate(
-        zip(examples["answers"], examples["is_impossible"])
-    ):
+
+    for i, (answers, unanswerable) in enumerate(zip(examples["answers"], examples["is_impossible"])):
         answerable = not unanswerable
-        assert (
-            len(answers["text"]) == len(answers["answer_start"]) or
-            answers["answer_start"][0] == -1
-        )
+        assert len(answers["text"]) == len(answers["answer_start"]) or answers["answer_start"][0] == -1
         if answerable and len(answers["text"]) > 1:
             for n_ans in range(len(answers["text"])):
                 ans = {
@@ -83,40 +70,29 @@ def data_aug_for_multiple_answers(examples) -> Union[Dict, Any]:
             update(i, {"text": [], "answer_start": []})
         else:
             update(i)
-            
+
     return result
 
-
 def main(args):
-    # Load SQuAD V2.0 dataset
     print("Loading SQuAD v2.0 dataset ...")
     squad_v2 = datasets.load_dataset("squad_v2")
-    # Integrate into the schema used in this library
-    # Note: The columns used for preprocessing are `question`, `context`, `answers`
-    #       and `is_impossible`. The remaining columns are columns that exist to 
-    #       process other types of data.
-    
-    # Minize the dataset for debugging
+
     if args.debug:
         squad_v2["train"] = squad_v2["train"].select(range(5))
         squad_v2["validation"] = squad_v2["validation"].select(range(5))
-    
+
     print("Integrating into the schema used in this library ...")
     squad_v2 = squad_v2.map(
-        schema_integrate, 
+        schema_integrate,
         batched=True,
         remove_columns=squad_v2.column_names["train"],
         features=EXAMPLE_FEATURES,
     )
-    # num_rows in train: 130,319, num_unanswerable in train: 43,498
-    # num_rows in valid:  11,873, num_unanswerable in valid:  5,945
     num_unanswerable_train = sum(squad_v2["train"]["is_impossible"])
     num_unanswerable_valid = sum(squad_v2["validation"]["is_impossible"])
     logger.warning(f"Number of unanswerable sample for SQuAD v2.0 train dataset: {num_unanswerable_train}")
     logger.warning(f"Number of unanswerable sample for SQuAD v2.0 validation dataset: {num_unanswerable_valid}")
-    # Train data augmentation for multiple answers
-    # no answer {"text": [], "answer_start": [-1]} -> {"text": [], "answer_start": []}
-    
+
     print("Data augmentation for multiple answers ...")
     squad_v2_train = squad_v2["train"].map(
         data_aug_for_multiple_answers,
@@ -125,14 +101,10 @@ def main(args):
         num_proc=5,
     )
     squad_v2 = datasets.DatasetDict({
-        "train": squad_v2_train,              # num_rows: 130,319
-        "validation": squad_v2["validation"]  # num_rows:  11,873
+        "train": squad_v2_train,
+        "validation": squad_v2["validation"]
     })
-    # Load Retro Reader
-    # features: parse arguments
-    #           make train/eval dataset from examples
-    #           load model from 🤗 hub
-    #           set sketch/intensive reader and rear verifier
+
     print("Loading Retro Reader ...")
     retro_reader = RetroReader.load(
         train_examples=squad_v2["train"],
@@ -140,15 +112,15 @@ def main(args):
         config_file=args.configs,
         device="cuda" if torch.cuda.is_available() else "cpu",
     )
+
+    # Resume from checkpoint by updating model weights from path manually
     if args.resume_checkpoint:
-        retro_reader = retro_reader.load_checkpoint(args.resume_checkpoint)
-    
-    # Train
+        retro_reader.model.load_state_dict(torch.load(os.path.join(args.resume_checkpoint, 'pytorch_model.bin')))
+
     print("Training ...")
     retro_reader.train(module=args.module)
     logger.warning("Train retrospective reader Done.")
-    
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--configs", "-c", type=str, default="configs/train_distilbert.yaml", help="config file path")
