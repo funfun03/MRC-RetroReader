31d2059cec75643ec1896925d454d08f025f4536
Phuong <phuong9agt@gmail.com>
2025-04-24 22:29:11 +0700

    Upload inference_script.py

---
+++ inference_script.py
@@ -0,0 +1,26 @@
+import json
+import datasets
+from retro_reader import RetroReader
+
+# Load dev set
+dataset = datasets.load_dataset("squad_v2", split="validation")
+
+# Load model
+reader = RetroReader.load(
+    config_file="E:\WORKSPACE\RetroReader\MRC-RetroReader\configs\inference_en_roberta.yaml",
+    device="cpu"
+)
+
+# Tạo predictions
+preds = {}
+for example in dataset:
+    qid = example["id"]
+    context = example["context"]
+    question = example["question"]
+    outputs = reader(query=question, context=context)
+    answer = outputs[0].get("id-01", "")
+    preds[qid] = answer if answer else ""
+
+# Lưu predictions
+with open("predictions.json", "w") as f:
+    json.dump(preds, f)
